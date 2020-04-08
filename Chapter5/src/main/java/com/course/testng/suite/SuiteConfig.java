9d7de5e115fad9bcafd53cb3fecc349057b0a833
guo333 <guo6262290@163.com>
2020-04-08 15:31:16 +0800

    Test

--- Chapter5/src/main/java/com/course/testng/suite/SuiteConfig.java
+++ Chapter5/src/main/java/com/course/testng/suite/SuiteConfig.java
@@ -1,4 +1,22 @@
 package com.course.testng.suite;
 
+import org.testng.annotations.*;
+
 public class SuiteConfig {
+    @BeforeSuite
+    public  void beforeSuite(){
+        System.out.println("before Suite运行啦！");
+    }
+    @AfterSuite
+    public void afterSuite(){
+        System.out.println("after Suite运行啦！");
+    }
+    @BeforeTest
+    public void beforetest(){
+        System.out.println("beforeTest");
+    }
+    @AfterTest
+    public void aftertest(){
+        System.out.println("afterTest");
+    }
 }
