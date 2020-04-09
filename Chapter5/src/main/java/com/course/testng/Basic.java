7fd1622c06a6936df436244aebe45a5c1d6d7ade
guo333 <guo6262290@163.com>
2020-04-09 16:49:44 +0800

    提交Chapter5

--- Chapter5/src/main/java/com/course/testng/Basic.java
+++ Chapter5/src/main/java/com/course/testng/Basic.java
@@ -5,10 +5,12 @@ public class Basic {
     //最基本的注解，用来把方法标记为测试的一部分
     @Test
     public void testCase1(){
+        System.out.printf("Thread ID:%s%n",Thread.currentThread().getId());
         System.out.println("这是测试用例1");
     }
     @Test
     public void testCase2(){
+        System.out.printf("Thread ID:%s%n",Thread.currentThread().getId());
         System.out.println("这是测试用例2");
     }
     @BeforeMethod
